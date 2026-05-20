a46b0c6e378d6b22670d48030b8fb75573dba2bd
takamori-saigo <anamneshev1999@gmail.com>
2026-05-20 23:23:50 +0500

    done program.cs

--- EquipmentWarehouse.Web/Program.cs
+++ EquipmentWarehouse.Web/Program.cs
@@ -1,6 +1,33 @@
+using EquipmentWarehouse.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.MapControllers();
+app.MapHub<WarehouseHub>("/warehouseHub");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<dbContext>();
+    context.Database.EnsureCreated();
+}
 app.Run();
